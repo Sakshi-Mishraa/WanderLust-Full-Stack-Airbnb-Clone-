c12d38a2a676f32b447606cdb164ac0c4b3e0a1a
Sakshi-Mishraa <mishrasakshi16439@gmail.com>
2025-09-23 01:41:06 +0530

    Fixed session expires for Render HTTPS

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const sessionOptions={
     resave:false,
     saveUninitialized:true,
     cookie:{
-         expires: new Date(Date.now() + 7*24*60*60*1000),
+        expires: new Date(Date.now() + 7*24*60*60*1000),
         maxAge:7*24*60*60*1000,
         httpOnly:true,
         secure: process.env.NODE_ENV === "production",
